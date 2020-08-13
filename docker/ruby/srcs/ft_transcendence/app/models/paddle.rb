a61911ee6bd64561a39bf94a29fba0385740a89b
ska42 <skalyxe00@gmail.com>
2020-08-13 18:53:02 +0200

    start paddle

--- docker/ruby/srcs/ft_transcendence/app/models/paddle.rb
+++ docker/ruby/srcs/ft_transcendence/app/models/paddle.rb
@@ -1,6 +1,32 @@
 class Paddle < ApplicationRecord
 	def initialize()
 		@posY = 50
-		@size = 10
+		@height = 10
+		@width = 2
+		@velocity = 1
+	end
+
+	def	up()
+		@posY += (1 * velocity);
+	end
+
+	def down()
+		@posX -= (1 * velocity);
+	end
+
+	def posY()
+		@posY
+	end
+
+	def height()
+		@height
+	end
+
+	def width()
+		@width
+	end
+
+	def velocity()
+		@velocity
 	end
 end
