5d3960b1c9a9f907586965ae7d290b72994bc969
ska42 <skalyxe00@gmail.com>
2020-08-17 18:44:25 +0200

    ragequit delete game

--- docker/ruby/srcs/ft_transcendence/app/javascript/channels/game_channel.js
+++ docker/ruby/srcs/ft_transcendence/app/javascript/channels/game_channel.js
@@ -1,65 +1,73 @@
 import consumer from "./consumer"
 
 document.addEventListener('turbolinks:load', () => {
-  var canvas = document.querySelector('.myCanvas');
-  if (canvas)
-  {
-    canvas.width = 600;
-    canvas.height = 600;
-    var ctx = canvas.getContext('2d');
-    ctx.fillStyle = "blue";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "black";
+	var canvas = document.querySelector('.myCanvas');
+	if (canvas)
+	{
+		canvas.width = 600;
+		canvas.height = 600;
+		var ctx = canvas.getContext('2d');
+		ctx.fillStyle = "blue";
+		ctx.fillRect(0, 0, canvas.width, canvas.height);
+		ctx.fillStyle = "black";
 
-    function printBall(x, y, radius)
-    {	  
-      ctx.beginPath();
-      ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
-      ctx.fill();
-    }
+		function printBall(x, y, radius)
+		{	  
+			ctx.beginPath();
+			ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
+			ctx.fill();
+		}
 
-    function printPaddle(x, y, width, height)
-    {
-      ctx.fillRect(x, y, width, height);
-    }
+		function printPaddle(x, y, width, height)
+		{
+			ctx.fillRect(x, y, width, height);
+		}
 
-    var sub = consumer.subscriptions.create({
-		channel: "GameChannel",
-		game: $('.GameInfo').attr("value")
-	}, {
-      connected() {
-		  // PADDLE
-        document.addEventListener('keypress', logKey);
-        function logKey(e)
-        {
-          if (e.key == 'w')
-            sub.perform('paddle_up', {});  
-  		    if (e.key == 's')
-            sub.perform('paddle_down', {});  
-        }
+		var sub = consumer.subscriptions.create({
+			channel: "GameChannel",
+			game: $('.GameInfo').attr("value")
+		}, {
+			connected() {
+				// PADDLE
+				document.addEventListener('keypress', logKey);
+				function logKey(e)
+				{
+					if (e.key == 'w')
+						sub.perform('paddle_up', {});  
+					if (e.key == 's')
+						sub.perform('paddle_down', {});  
+				}
 
-    	  //REQUEST UPDATE
-        setInterval(function() {
-    	   	sub.send({});
-    		}, 40);
-      },
+				//REQUEST UPDATE
+				setInterval(function() {
+					sub.send({});
+				}, 40);
+			},
 
-      disconnected() {
+			disconnected() {
 
-      },
+			},
 
-      received(data) {
-    		$("#game_status").html(data.status);
-        $("#p1_pts").html(data.player1_pts);
-        $("#p2_pts").html(data.player2_pts);
-        ctx.fillStyle = "blue";
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-        ctx.fillStyle = "black";
- 		printBall(data.ballPosX, data.ballPosY, data.ballRadius);
-        printPaddle(data.paddle1PosX, data.paddle1PosY, data.paddle1Width, data.paddle1Height);
-        printPaddle(data.paddle2PosX, data.paddle2PosY, data.paddle2Width, data.paddle2Height);
-      }
-    });
-  }
+			received(data) {
+				if (data.winner)
+				{
+					alert("Player "+data.winner+" win");
+					sub.perform('end', {});
+				}
+				else
+				{
+					$("#game_status").html(data.status);
+					$("#p1_pts").html(data.player1_pts);
+					$("#p2_pts").html(data.player2_pts);
+					ctx.fillStyle = "blue";
+					ctx.clearRect(0, 0, canvas.width, canvas.height);
+					ctx.fillRect(0, 0, canvas.width, canvas.height);
+					ctx.fillStyle = "black";
+					printBall(data.ballPosX, data.ballPosY, data.ballRadius);
+					printPaddle(data.paddle1PosX, data.paddle1PosY, data.paddle1Width, data.paddle1Height);
+					printPaddle(data.paddle2PosX, data.paddle2PosY, data.paddle2Width, data.paddle2Height);
+				}
+			}
+		});
+	}
 });
