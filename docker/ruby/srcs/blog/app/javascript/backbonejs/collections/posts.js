1f59c0eb9db0a9a3be345874dec7a32c8b6bbc31
hexanyn <hexanyn@gmail.com>
2020-08-17 23:14:08 +0200

    FUCKING BACKBONE WORKING

--- docker/ruby/srcs/blog/app/javascript/backbonejs/collections/posts.js
+++ docker/ruby/srcs/blog/app/javascript/backbonejs/collections/posts.js
@@ -2,3 +2,5 @@ window.app.collections.Posts = Backbone.Collection.extend({
 	url: '/api/posts',
 	model: window.app.models.Post
 });
+
+window.app.collections.newPosts = new window.app.collections.Posts;
--- docker/ruby/srcs/blog/app/javascript/backbonejs/models/post.js
+++ docker/ruby/srcs/blog/app/javascript/backbonejs/models/post.js
@@ -1,2 +1,4 @@
 window.app.models.Post = Backbone.Model.extend({
 });
+
+window.app.models.newPost = new window.app.models.Post;
