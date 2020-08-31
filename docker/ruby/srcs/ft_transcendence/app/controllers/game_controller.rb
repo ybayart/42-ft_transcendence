e8679017e177070273a935354ad83be969961501
fgoulama <fgoulama@student.42.fr>
2020-08-31 12:57:02 +0200

    small cleaning

--- docker/ruby/srcs/ft_transcendence/app/controllers/game_controller.rb
+++ docker/ruby/srcs/ft_transcendence/app/controllers/game_controller.rb
@@ -21,27 +21,12 @@ class GameController < ApplicationController
   end
 
   def find_game
-    @game = Game.find_by(status: "waiting", player1: current_user);
-    @game ||= Game.find_by(status: "running", player1: current_user);
-    if @game
-      @game.destroy
-      @game = nil
-    end
-    @game = Game.find_by(status: "running", player2: current_user);
-    @game ||= Game.find_by(status: "waiting", player2: current_user);
-    if @game
-      @game.destroy
-      @game = nil
-    end
-    @game ||= Game.find_by(status: "waiting");
-    if @game
-      @game.player2 = current_user
+    @game = Game.find_by("player1_id != ? AND status = ?", current_user, "waiting");
+    if !@game
+      @game = Game.create(player1: current_user, status: "waiting");
+    else
+      @game.player2 = current_user;
       @game.status = "running"
-      @game.save
-    elsif !@game
-      @game = Game.new
-      @game.status = "waiting"
-      @game.player1 = current_user
       @game.save
     end
     @game
