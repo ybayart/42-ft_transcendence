c2aa7bdc5672f4494c515463f8a576cb5f5ebfc3
fgoulama <fgoulama@student.42.fr>
2020-09-02 23:12:34 +0200

    delete not wanted files

--- docker/ruby/srcs/ft_transcendence/app/views/tournaments/_tournament.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! tournament, :id, :created_at, :updated_at
-json.url tournament_url(tournament, format: :json)
--- docker/ruby/srcs/ft_transcendence/db/migrate/20200902122700_create_tournaments.rb
+++
@@ -1,8 +0,0 @@
-class CreateTournaments < ActiveRecord::Migration[6.0]
-  def change
-    create_table :tournaments do |t|
-
-      t.timestamps
-    end
-  end
-end
