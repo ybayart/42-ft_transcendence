dcd1a3c87a1e7346496d423a16bdc90c31d55273
fgoulama <fgoulama@student.42.fr>
2020-09-07 16:50:18 +0200

    remove cursed migration

--- docker/ruby/srcs/ft_transcendence/db/migrate/20200907120945_fix_winner_in_tournaments.rb
+++
@@ -1,6 +0,0 @@
-class FixWinnerInTournaments < ActiveRecord::Migration[6.0]
-  def change
-  	remove_reference(:tournaments, :winner_id)
-  	add_reference(:tournaments, :winner, foreign_key: {to_table: :users})
-  end
-end
