e4fb2d74360049ad4fab65aa5449b14e2716c8bd
fgoulama <fgoulama@student.42.fr>
2020-09-08 18:35:31 +0200

    dont show status on own profil

--- docker/ruby/srcs/ft_transcendence/db/seeds.rb
+++ docker/ruby/srcs/ft_transcendence/db/seeds.rb
@@ -5,5 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-Game.create(player1_id: nil, player2_id: nil, status: "empty")
