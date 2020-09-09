2d83dd9029e77e89953a0a9242c85c5f1ba50ddf
hexanyn <hexanyn@gmail.com>
2020-09-09 20:03:37 +0200

    hello world

--- docker/ruby/srcs/ft_transcendence/app/models/guild.rb
+++ docker/ruby/srcs/ft_transcendence/app/models/guild.rb
@@ -8,6 +8,7 @@ class Guild < ApplicationRecord
 	has_many :members, class_name: "User", foreign_key: "guild_id", inverse_of: :guild
 
 	validates	:anagram, presence: true, length: { maximum: 5 }
+	validates	:name, presence: true
 	validate	:check_columns
 
 	after_commit	:apply_modifications
